4aab59d7b77c438f594f6a7d94f0acd94c4d5960
Luiz Herkenhoff Coelho <luizherkenhoffcoelho@iMac-de-Luiz.local>
2016-05-09 10:21:52 +0200

    Project creation

---
+++ PrincipalStress/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PrincipalStress
+//
+//  Created by Luiz Herkenhoff Coelho on 09/05/16.
+//  Copyright © 2016 Luiz Herkenhoff Coelho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
