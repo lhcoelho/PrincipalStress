888db3adf4e8f544bce5873af582d5599be8f530
Luiz Herkenhoff Coelho <luizherkenhoffcoelho@iMac-de-Luiz.local>
2016-05-09 11:54:03 +0200

    first UI with button

--- PrincipalStress/ViewController.swift
+++ PrincipalStress/ViewController.swift
@@ -9,7 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var sigmaX: UITextField!
+    @IBOutlet weak var sigmaY: UITextField!
+    @IBOutlet weak var sigmaZ: UITextField!
+    
+    @IBOutlet weak var tauXY: UITextField!
+    @IBOutlet weak var tauXZ: UITextField!
+    @IBOutlet weak var tauYZ: UITextField!
+    
+    @IBOutlet weak var sigma1: UILabel!
+    @IBOutlet weak var sigma2: UILabel!
+    @IBOutlet weak var sigma3: UILabel!
+    @IBOutlet weak var tauMax: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +32,6 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    
 }
 
